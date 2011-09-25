160e0a052a85ee96d985633f28a7a02115cf52db
eghm <github-m@eghm.net>
2011-09-24 19:22:34 -0700

    draft implementation

---
+++ NoTldJars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# one jar per/line
+grep "No.TLD.files.were.found.in" $1 | cut -d: -f 3 | cut -d] -f 1 | sed 's|/| |g' | awk '{print $NF}' | perl -e 'undef $/; print join( ",\\\n", split /\n/, <STDIN> ), "\n"'
+
+# one line
+#grep "No.TLD.files.were.found.in" $1 | cut -d: -f 3 | cut -d] -f 1 | sed 's|/| |g' | awk '{print $NF}' | paste -s -d, - | sed 's|,\\||g'
+
